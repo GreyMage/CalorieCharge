d686ded5ae4c8ec091f17c28018c04cef17f7442
Cades <bulzeeb@gmail.com>
2014-09-14 20:21:10 -0400

    somehow lost a fuckload of work so yeah ok rewriting this shit

--- public/js/main.js
+++ public/js/main.js
@@ -2,64 +2,6 @@ $( "#cmeter" ).progressbar({
 	value: false
 });
 
-
-$.CalorieCharge = function(){
-
-	var defaultDailyRate = 1700;
-
-	this._getOrCreateLS = function(name,def){
-		var x = localStorage.getItem(name);
-		if(x === null){
-			localStorage.setItem(name, def);
-			return def;
-		}
-		return x;
-	}
-	this._setLS = function(name,def){
-		localStorage.setItem(name, def);
-		console.log("setting",name,"to",def);
-	}
-	this._modLS = function(name,x){
-		var orig = this._getOrCreateLS(name,0);
-		this._setLS(name, parseFloat(orig)+parseFloat(x));
-	}
-
-	this.getDailyRate = function(){
-		return this._getOrCreateLS("dailyChargeRate",defaultDailyRate);
-	}
-	this.getMilliRate = function(){
-		var daily = this._getOrCreateLS("dailyChargeRate",defaultDailyRate);
-		var hourly = daily / 24;
-		var minutely = hourly / 60;
-		var secondly = minutely / 60;
-		var millily = secondly / 1000;
-		return millily;
-	}
-	this.getCurrent = function(modify){
-	
-		// Get time since last check, and update "last" to be now.
-		var last = this._getOrCreateLS("lastCheck",new Date().getTime());
-		var now = new Date().getTime();
-		var delta = now - last;
-		
-		if(modify) this._setLS("lastCheck",now);
-		
-		//delta represents the millis passed since the last check. 
-		// Take milli rate and multiply.
-		var charged = delta * this.getMilliRate();
-		var value = parseFloat(this._getOrCreateLS("currentCharge",this.getDailyRate())) + charged;
-		value = Math.min(this.getDailyRate(),value);
-		
-		if(modify) this._setLS("currentCharge",value);
-		
-		// Return Current after accounting for timechanges.
-		return {
-			numeric: value,
-			fraction: ( value / this.getDailyRate() )
-		}
-	}
-}
-
 var roundTo = function(number,places){
 
 	number = Math.pow(10,places) * number;
@@ -73,27 +15,54 @@ var roundTo = function(number,places){
 	return s;
 }
 
-var cc = new $.CalorieCharge();
+ccBar = {};
+ccBar.rate = 250;
+ccBar.millirate = 0;
+ccBar.numeric = 0;
+ccBar.total = 0;
+ccBar._update = 0;
+ccBar._last = new Date().getTime();
 
-$(function() {
-    $(window).focus(function() {
-        window.hasFocus = true;
-    });
-
-    $(window).blur(function() {
-        window.hasFocus = false;
-    });
-	
-	window.hasFocus = true;
-});
-
-var update = function(hmm){
-	var stats = cc.getCurrent(hmm);
-	$( "#cmeter" ).progressbar( "value", stats.fraction*100 );
-	$( "#cmeter .label" ).text( roundTo(stats.numeric,3) + " Calories" );
+ccBar.getRaw = function(){
+	$.post("/data",function(data){
+		console.log(data);
+		ccBar.millirate = data.data.millirate;
+		ccBar.numeric = data.data.numeric;
+		ccBar.total = data.data.total;
+		ccBar.update();
+	});
 }
 
-update(true);
-setInterval(function(){
-	update(false);
-},250);
+ccBar.update = function(){
+	clearTimeout(ccBar._update);
+
+	// Get delta
+	var now = new Date().getTime();
+	var delta = now - ccBar._last;
+	ccBar._last = now;
+	ccBar.numeric += (delta * ccBar.millirate);
+
+	$( "#cmeter" ).progressbar( "value", ccBar.numeric / ccBar.total * 100 );
+	$( "#cmeter .label" ).text( roundTo(ccBar.numeric,3) + " Calories" );
+
+	setTimeout(function(){
+		ccBar.update();
+	},ccBar.rate)
+}
+
+var update = function(hmm){
+	//$( "#cmeter" ).progressbar( "value", stats.fraction*100 );
+	//$( "#cmeter .label" ).text( roundTo(stats.numeric,3) + " Calories" );
+}
+
+ccBar.getRaw();
+
+$(function(){
+	$("#menubtn").click(function(){
+		if($("#menu").is(":visible")){
+			$("#menu").hide("blind");
+		} else {
+			$("#menu").show("blind");
+		}
+	})
+})
